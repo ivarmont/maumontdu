9c456e225799f4a4521dddbe66794979b3ace779
Mauricio Montellano <ivar.montellano@gmail.com>
2023-04-15 10:22:16 +0200

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Home } from "./components/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import { About } from "./components/About";
 import { Navbar } from "./components/Navbar";
@@ -7,13 +7,13 @@ import { Navbar } from "./components/Navbar";
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
